c630ad2afdcf0a207fd12cb4c94cc10a42b3579f
JürgenWithWings <danraw6604@outlook.com>
2025-01-13 21:22:15 +0000

    Crazy cube not the scene

---
+++ Assets/TheCrazyScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TheCrazyScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Holy moly cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
