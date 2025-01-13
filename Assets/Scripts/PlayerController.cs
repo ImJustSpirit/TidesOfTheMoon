1f7b4162d4b57b500b97508e23037416aa3a2ab8
Jack Petty <77806303+ImJustSpirit@users.noreply.github.com>
2025-01-13 23:17:55 +0000

    Basic Bullet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int speed = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
- 
-    }
 
-    // Update is called once per frame
+    // Bullet Variables
+    public GameObject playerBullet;
+    public float shootCooldown = 0.2f;
+    private float shootTimer;
+    public float damage = 1;
+    public float bulletSpeed = 30;
+    private bool shootSide = false;
+    private float bulletOffset;
+
     void Update()
     {
+        // WASD
         if (Input.GetKey(KeyCode.W) && transform.position.y < 2) {
             transform.position += new Vector3(0, speed * Time.deltaTime, 0);
             if (transform.position.y > 2) { transform.position = new Vector3(transform.position.x, 2, transform.position.z); }
@@ -29,6 +33,30 @@ public class PlayerController : MonoBehaviour
         {
             transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
             if (transform.position.x < -4) { transform.position = new Vector3(-4, transform.position.y, transform.position.z); }
+        }
+
+        if (shootTimer < shootCooldown)
+        {
+            shootTimer += Time.deltaTime;
+        }
+
+        // Space
+        if (Input.GetKey(KeyCode.Space) && shootTimer>=shootCooldown)
+        {
+            shootTimer = 0;
+            if (shootSide)
+            {
+                bulletOffset = 1;
+                shootSide = false;
+            }
+            else
+            {
+                bulletOffset = -1;
+                shootSide = true;
+            }
+            GameObject newBullet = Instantiate(playerBullet, new Vector3(transform.position.x + bulletOffset, transform.position.y, transform.position.z), Quaternion.Euler(90, 0, 0));
+            newBullet.GetComponent<Bullet>().damage = 1;
+            newBullet.GetComponent<Rigidbody>().linearVelocity = Vector3.forward * bulletSpeed;
         }
     }
 }
