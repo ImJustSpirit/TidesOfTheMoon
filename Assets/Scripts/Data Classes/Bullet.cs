5fe0d1d1d807489d1e7bb7f7a4912bdf1b3a9381
Oliver Neal <oliverjn04@gmail.com>
2025-01-14 20:52:31 +0000

    Changed Ship Model + Bullet Timeout

--- Assets/Scripts/Data Classes/Bullet.cs
+++ Assets/Scripts/Data Classes/Bullet.cs
@@ -4,4 +4,14 @@ using UnityEngine.Rendering;
 public class Bullet : MonoBehaviour
 {
     public float damage;
+
+    private void Start()
+    {
+        Invoke("Timeout", 5);
+    }
+
+    private void Timeout()
+    {
+        Destroy(gameObject);
+    }
 }
